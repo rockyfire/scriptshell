f9a6a3fe7cd51db661e79a66a235b256fd8d94b4
rockyfire <rockyfire@126.com>
2017-10-24 17:11:48 +0800

    judge directory

--- download_hadoop.sh
+++ download_hadoop.sh
@@ -1,11 +1,11 @@
-######################################
-# Author : rockyfire
-# !/bin/bash
-
-for i in {1901..2016}
-do
-	cd /linuxtext
-	wget -r -np -nH --cut-dirs=3 -R index.html ftp://ftp.ncdc.noaa.gov/pub/data/noaa/isd-lite/$i/
-	cd isd-lite/$i
-	gzip -d *.gz
+#!/bin/bash 
+for i in {1901..2017} 
+do     
+	cd /linuxtext/isd-lite     
+	if [ -d $i ]; then         
+		continue     
+	fi     
+	wget -r -np -nH --cut-dirs=4 -R index.html ftp://ftp.ncdc.noaa.gov/pub/data/noaa/isd-lite/$i/     
+	cd $i     
+	gzip -d *.gz 
 done
